36fd91b00829421f461e5877a929eef0e6ccc3ae
razifp82 <razifp82@gmail.com>
2023-12-07 21:00:54 +0700

    all

--- relawan/notif.php
+++ relawan/notif.php
@@ -1,1 +1,10 @@
-<i class="bi bi-bell-fill"></i>
+<div class="my-moon"></div>
+  <div class="notification-box">
+    <span class="notification-count">6</span>
+    <div class="notification-bell">
+      <span class="bell-top"></span>
+      <span class="bell-middle"></span>
+      <span class="bell-bottom"></span>
+      <span class="bell-rad"></span>
+    </div>
+  </div>
